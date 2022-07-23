04515b62a9418b1a99b8e6ac769dc3a9db27812a
Mark55Kelly <108310217+Mark55Kelly@users.noreply.github.com>
2022-07-23 04:15:23 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Assignment code here
-
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var lowercase = ['a', 'b', 'c', 'd']
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
