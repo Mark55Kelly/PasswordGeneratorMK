d516652618336aa737b369371901f89932cd43e7
Mark55Kelly <108310217+Mark55Kelly@users.noreply.github.com>
2022-07-23 05:24:12 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 // Assignment code here
 var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-var lowercase = ['a', 'b', 'c', 'd']
+var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var specialCharacters = []
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 console.log(generateBtn);
 
 // Write password to the #password input
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -15,6 +18,11 @@ function writePassword() {
 
 }
 
+function generatePassword() 
+
+prompt ("How many characters would you like in your password?");
+if (passwordLength < 8) stop function. 
+if (passwordLength > 128) stop function 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
