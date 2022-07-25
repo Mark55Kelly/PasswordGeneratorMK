7e5d174dc75dbd72903356a0f221faf479344b09
Mark55Kelly <108310217+Mark55Kelly@users.noreply.github.com>
2022-07-24 23:59:10 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -8,12 +8,18 @@ console.log (specialCharacters);
 
 console.log (lowercase[21]);
 console.log (specialCharacters[10]);
+
+var IncludeSpecialCharacters = confirm("click OK to confirm including special characters.")
+var IncludeNumericCharacters = confirm("Click OK to confirm including numeric characters")
+var IncludeLowercaseCharacters = confirm("Click OK to confirm including lowercase charcters")
+var IncludeUppercaseCharacters = confirm("Click OK to confirm including uppercase characters")
+var Characterlengthpass = prompt("How many characters would you like your password to contain?")
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 console.log(generateBtn);
 
 // Write password to the #password input
-
 
 function writePassword() {
   var password = generatePassword();
@@ -22,3 +28,20 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+//my change here.
+//notes: putting the prompt inside the function right away didn't work it made the window pop open.
+function generatePassword() {
+
+}
+
+generatePassword();
+//My change End.
+
+//prompt ("How many characters would you like in your password?");
+//if (passwordLength < 8) stop function. 
+//if (passwordLength > 128) stop function 
+
+// Add event listener to generate button
+
+generateBtn.addEventListener("click", writePassword);
