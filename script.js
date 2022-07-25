15f2e07758b82a05f8f4cab423f957939aa5a721
Mark55Kelly <108310217+Mark55Kelly@users.noreply.github.com>
2022-07-25 00:24:32 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,22 +2,29 @@
 var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 var specialCharacters = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', '{', ']', '}', ',', '<', '.', '>', '/', '?', ';', ':', '"']
+var numericValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
 console.log (uppercase);
 console.log (lowercase);
 console.log (specialCharacters);
+console.log (numericValues)
 
 console.log (lowercase[21]);
 console.log (specialCharacters[10]);
+
+//all of these need to be within the button being generated so that it takes place within that object and not outside of it in the window like it just did there... I think I'm starting to get this shit now. 
+
 
 var IncludeSpecialCharacters = confirm("click OK to confirm including special characters.")
 var IncludeNumericCharacters = confirm("Click OK to confirm including numeric characters")
 var IncludeLowercaseCharacters = confirm("Click OK to confirm including lowercase charcters")
 var IncludeUppercaseCharacters = confirm("Click OK to confirm including uppercase characters")
-var Characterlengthpass = prompt("How many characters would you like your password to contain?")
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-console.log(generateBtn);
+console.log(generateBtn); 
+
 
 // Write password to the #password input
 
@@ -32,6 +39,9 @@ function writePassword() {
 //my change here.
 //notes: putting the prompt inside the function right away didn't work it made the window pop open.
 function generatePassword() {
+  var CharacterCount = prompt("How many characters would you like your password to contain?");
+  if (CharacterCount >7)
+  console.log("could this be progress?");
 
 }
 
